064d0d67ae7867c7b04a2a485dfa378580e4d104
Priyansh Khandal <139154371+iamuserPriyansh@users.noreply.github.com>
2023-12-25 18:43:33 +0530

    Update terminalconfig.sh

--- terminalconfig.sh
+++ terminalconfig.sh
@@ -1,2 +1,3 @@
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+#git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+#cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
